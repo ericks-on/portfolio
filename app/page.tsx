b51d7b8e5f1a908ddcc220d5ff59a1a9fc617b9d
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-12-10 22:05:55 +0300

    flex end

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div >
       {/* landing */}
-      <div className="flex flex-col-reverse justify-flex-end md:flex-row pt-20 h-screen w-screen gap-4">
+      <div className="flex flex-col-reverse justify-end md:flex-row pt-20 h-screen w-screen gap-4">
         <HomeSummary />
         <Image 
         className="h-1/2 w-full md:h-full md:w-2/5"
